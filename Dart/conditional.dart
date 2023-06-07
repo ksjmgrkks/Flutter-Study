78cea05589573d545a11f526d41240dc5f75f8f7
Kyu Seong Kim <ksjmgrkks@gmail.com>
2023-06-07 19:38:43 +0900

    Update conditional.dart

--- Dart/conditional.dart
+++ Dart/conditional.dart
@@ -5,6 +5,30 @@ void main() {
   if (number % 2 == 0) {
     print('짝수');  
   } else {
-    print('홀수');
+    print('홀수'); // 출력
   }
+  
+  if (number % 3 == 0) {
+    print('나머지 0'); // 출력
+  } else if(number % 3 == 1) {
+    print('나머지 1'); 
+  } else {
+    print('나머지 2'); 
+  }
+  
+  //switch 문
+  switch(number % 3) {
+      case 0:
+        print('나머지 0'); // 출력
+        break;
+
+      case 1:
+        print('나머지 1');
+        break;
+
+      default: //if문에서의 else
+        print('나머지 2');
+        break;
+  }
+  
 }
