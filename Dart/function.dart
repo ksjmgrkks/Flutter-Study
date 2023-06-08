638d59b678ed41a82c3ae3b1382528a820710414
Kyu Seong Kim <ksjmgrkks@gmail.com>
2023-06-08 11:12:04 +0900

    Update function.dart

--- Dart/function.dart
+++ Dart/function.dart
@@ -5,6 +5,18 @@ void main() {
   addNumbersOptional(10);
   addNumbersOptional(10, 20);
   addNumbersOptional(10, 20, 30);
+  
+  addNumbersNamed(x: 10, y: 20, z: 30); // 결과 동일
+  addNumbersNamed(y: 20, x: 10, z: 30); // 결과 동일 
+  
+  addNumbersNamed(y: 20, x: 10); // z == 0
+  
+  addNumbersMixed(10, y: 10);
+  
+  int result1 = addNumbersReturnSum(10, 20 , 30);
+  int result2 = addNumbersReturnSum(10, 30 , 40);
+  
+  print('sum : ${result1 + result2}'); // sum : 140
 }
 
 // 세개의 숫자 (x, y, z)를 더하고 짝수인지 홀수인지 알려주는 함수
@@ -29,3 +41,23 @@ addNumbers(int x, int y, int z) {
 addNumbersOptional(int x, [int y = 0, int z = 0]){ //0은 y,z 의 기본값!
   addNumbers(x, y, z);
 }
+
+// named parameter - 이름이 있는 파라미터 (순서가 중요하지 않음)
+// required 를 빼면 optional이 됨
+addNumbersNamed({
+  required int x,
+  required int y,
+  int z = 0,
+}) {
+  addNumbers(x, y, z);
+}
+
+addNumbersMixed(int x, {
+  required int y,
+  int z = 0,
+}) {
+  addNumbers(x, y, z);
+}
+
+// arrow function
+int addNumbersReturnSum(int x, int y, int z) => x + y + z;
