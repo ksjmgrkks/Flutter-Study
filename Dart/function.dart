9124de52e01549a58820d0d88c797775c619db0d
Kyu Seong Kim <ksjmgrkks@gmail.com>
2023-06-08 10:55:24 +0900

    Update function.dart
    optional parameter

--- Dart/function.dart
+++ Dart/function.dart
@@ -2,6 +2,9 @@ void main() {
   addNumbers(10, 20, 30);
   addNumbers(20, 35, 40);
 
+  addNumbersOptional(10);
+  addNumbersOptional(10, 20);
+  addNumbersOptional(10, 20, 30);
 }
 
 // 세개의 숫자 (x, y, z)를 더하고 짝수인지 홀수인지 알려주는 함수
@@ -20,4 +23,9 @@ addNumbers(int x, int y, int z) {
     print('합이 홀수입니다.');
   }
 
+}
+
+// optional parameter - 있어도 되고 없어도 되는 파라미터
+addNumbersOptional(int x, [int y = 0, int z = 0]){ //0은 y,z 의 기본값!
+  addNumbers(x, y, z);
 }
