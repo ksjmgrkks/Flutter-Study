8ccef2584ddca278b4c4ce629275fc7155755489
kyuseongkim <ksjmgrkks@naver.com>
2023-06-14 11:08:30 +0900

    [riverpod] upload and intro

---
+++ riverpod_project/lib/screen/state_provider_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:untitled/layout/default_layout.dart';
+import 'package:untitled/riverpod/state_provider_screen.dart';
+
+class StateProviderScreen extends ConsumerWidget {
+  const StateProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider); /* 화면이 바뀌어도 값이 공유됨(SSOT) */
+
+    return DefaultLayout(
+      title: 'StateProviderScreen',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: Text('+1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => _NextScreen(),
+                  ),
+                );
+              },
+              child: Text('Go To NextScreen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider); /* 화면이 바뀌어도 값이 공유됨(SSOT) */
+
+    return DefaultLayout(
+      title: 'NextScreen',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state - 1);
+              },
+              child: Text('-1'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
